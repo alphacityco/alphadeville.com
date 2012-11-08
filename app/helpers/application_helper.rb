972d33c5b9a165dcc422c08d125ad56f30d558f8
Raul Pino <pinox101@gmail.com>
2012-11-07 21:23:03 -0430

    testing twt btn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def tweet_anchor( data_text )
     t_a = <<-STR
       <div class="tweet-btn pull-right">
-        <a href="https://twitter.com/share" class="twitter-share-button" data-via="alphadeville" data-text="#{data_text}" data-lang="en" data-count="none">Tweet</a>
+        <a href="https://twitter.com/share" class="twitter-share-button" data-url="none" data-via="alphadeville" data-text="#{data_text}" data-lang="en" data-count="none">Tweet</a>
       </div>
     STR
     t_a.html_safe
