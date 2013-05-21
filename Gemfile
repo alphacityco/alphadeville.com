e93b0d3a9d62bea8fc42987e4cb5a5f6ba079920
Raul Pino <pinox101@gmail.com>
2013-05-21 10:28:14 -0430

    display changes on shocase and activeadmin installed

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ gem 'rails', '3.2.13'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
+# admin
+gem 'activeadmin'
+
 # system
 gem 'thin'
 
---
+++ app/admin/dashboard.rb
@@ -0,0 +1,15 @@
+ActiveAdmin.register_page "Dashboard" do
+
+  menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
+
+  content :title => proc{ I18n.t("active_admin.dashboard") } do
+    div :class => "blank_slate_container", :id => "dashboard_default_message" do
+      span :class => "blank_slate" do
+        span "Interfaz Administrativa de Alphadeville"
+        br
+        img src: "/assets/logo/logo_big_black_li.png"
+      end
+    end    
+  end
+
+end
