0d232f83762c1653ff24782b8899adfd7b6d4a50
Raul Pino <pinox101@gmail.com>
2013-05-25 13:22:36 -0430

    no need of init lightbox

--- app/assets/javascripts/lib/alpha_theme.script.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function(){
-	// $('.pic a').lightBox({
-	// 	imageLoading: '/assets/lightbox/loading.gif',
-	// 	imageBtnClose: '/assets/lightbox/close.gif',
-	// 	imageBtnPrev: '/assets/lightbox/prev.gif',
-	// 	imageBtnNext: '/assets/lightbox/next.gif'
-	// });
-});
